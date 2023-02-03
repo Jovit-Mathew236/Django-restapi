e6459f175519da429db06ec10d4bfd54d5a9c3ce
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-02-04 02:33:23 +0530

    new restapi

---
+++ api/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+
+User = get_user_model()
+
+
+class UserRegister(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+
+    def save(self):
+        reg = User(
+            username=self.validated_data['username'],
+        )
+        password = self.validated_data['password']
+
+        reg.set_password(password)
+        reg.save()
+        return reg
+
+
+class LoginSerializers(serializers.Serializer):
+    name = serializers.CharField()
+    password = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+
+
+class UserDataSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
---
+++ api/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from .serializers import UserRegister, UserDataSerializer, LoginSerializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User, auth
+
+# Create your views here.
+
+class register(APIView):
+    
+    def post(self,request,format=None):
+        serializer=UserRegister(data=request.data)
+        data={}
+        if serializer.is_valid():
+            account=serializer.save()
+            data['response']='success'
+        else:
+            data=serializer.errors
+        return Response(data)
+
+class login(APIView):
+    
+    # def post(self,request,format=None):
+    #     serializer = LoginSerializers(data=request.data)
+    #     data = {}
+    #     if serializer.is_valid(raise_exception=True):
+    #         # account=serializer.save()
+    #         data['response'] = 'success'
+    #     else:
+    #         data = serializer.errors
+    #     return Response(data)    
+
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializers(data=request.data)
+        if serializer.is_valid():
+            name = request.POST['username']
+            password = request.POST['password']
+            # msg = 'Hello{}'.format(name)
+            user = auth.authenticate(username=name, password=password)
+            if user is not None:
+                return Response('success')
